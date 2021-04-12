ce554b64db786ab210478fdac693c3ac3ba28beb
Peisen <linpeisensh@gmail.com>
2021-04-12 20:21:41 +0200

    Readme

--- README.md
+++ README.md
@@ -1,56 +1,49 @@
-# stereo_ptam
+# RDR_SLAM
 
-This python project is a complete implementation of Stereo PTAM, based on C++ project [lrse/sptam](https://github.com/lrse/sptam) and paper "[S-PTAM: Stereo Parallel Tracking and Mapping](http://webdiis.unizar.es/~jcivera/papers/pire_etal_ras17.pdf) Taihu Pire et al. RAS17", with some modifications.   
+RDR_SLAM is a visual SLAM system that is robust in dynamic scenarios for stereo configurations. Having a static map of the scene allows inpainting the frame background that has been occluded by such dynamic objects.
 
-> S-PTAM is a Stereo SLAM system able to compute the camera trajectory in real-time. It heavily exploits the parallel nature of the SLAM problem, separating the time-constrained pose estimation from less pressing matters such as map building and refinement tasks. On the other hand, the stereo setting allows to reconstruct a metric 3D map for each frame of stereo images, improving the accuracy of the mapping process with respect to monocular SLAM and avoiding the well-known bootstrapping problem. Also, the real scale of the environment is an essential feature for robots which have to interact with their surrounding workspace.   
+<img src="imgs/teaser.png" width="900px"/>
 
-S-PTAM system overview (from [S-PTAM paper](http://webdiis.unizar.es/~jcivera/papers/pire_etal_ras17.pdf) page 11):  
-![](imgs/sptam_overview.png)
+RDR_SLAM: Semantic Segmentaton of Moving Objects   
 
-As stated in the [S-PTAM paper](http://webdiis.unizar.es/~jcivera/papers/pire_etal_ras17.pdf) (page 39), S-PTAM's results on KITTI dataset is comparable to stereo version of [ORB-SLAM2](https://github.com/raulmur/ORB_SLAM2), and better than stereo LSD-SLAM. It's very inspiring.
-
-
-## Features 
-(of this implementation)
-* Semantic Segmentation of Moving Objects
-* Multithreads Tracking, Mapping, and Loop Closing;
-* Covisibility Graph (representing the relation between keyframes, mappoints and measurements);
-* Local Bundle Adjustment and Pose Graph Optimization;
-* Motion Model (used for pose prediction, then for reliable feature matching);
-* Data loader for datasets [KITTI Odometry](http://www.cvlibs.net/datasets/kitti/eval_odometry.php) and [EuRoC MAV](http://projects.asl.ethz.ch/datasets/doku.php?id=kmavvisualinertialdatasets);
+We provide examples to run the SLAM system in the [KITTI dataset](http://www.cvlibs.net/datasets/kitti/eval_odometry.php) as stereo.
 
 
-## Requirements
-* Python 3.6
-* numpy
-* opencv-python, opencv-contrib-python  == 4.1.2.30
-* [g2o](https://github.com/uoip/g2opy) <sub>(python binding of C++ library [g2o](https://github.com/RainerKuemmerle/g2o))</sub> for optimization
-* apex
-* cocoapi
-* torchvision == 0.2.1
-* torch == 1.2.0
-* maskrcnn_benchmark
+## Getting Started
+- Install DynaSLAM (https://github.com/BertaBescos/DynaSLAM).
+- Install and run GANet (https://github.com/linpeisensh/GANet)
+- Install and run SNE-RoadSeg (https://github.com/linpeisensh/SNE-RoadSeg)
+- Clone this repo:
+```
+mkdir build
+cd build
+cmake ..
+make
+make install
+```
 
-## Usage
-`python dysptam.py --device cpu/cuda --no-viz --dataset kitti --path path/to/your/KITTI_odometry_dataset/sequences/00`  
+## Stereo Example on KITTI Dataset
+- Download the dataset (color images) from http://www.cvlibs.net/datasets/kitti/eval_odometry.php 
 
+- Execute the following command. Change `PATH_TO_ORB`to ORB_SLAM2 folder. Change `PATH_TO_SEQUENCES` to the uncompressed dataset folder. Change `DEVICE` to cpu or cuda. Change `SAVE` to 0 (not save) or 1. Change `SEQUENCE` to 00, 01, 02,.., 11. 
+```
+python RDR.py (PATH_TO_ORB) (PATH_TO_SEQUENCES) (DEVICE) (SAVE) (SEQUENCE) 
+```
+example:
+```
+python RDR.py (PATH_TO_ORB)/usr/stud/linp/storage/user/linp/ORB_SLAM2 cuda /storage/remote/atcremers17/linp/dataset/kittic/sequences/ 0 10(PATH_TO_MASKS)
+```
 
+## Citation
 
-### TODO:
-Exhaustive evaluation on datasets. (There seems to be a python package [MichaelGrupp/evo](https://github.com/MichaelGrupp/evo) for odometry/SLAM algorithm evaluation)
+If you use RDR_SLAM in an academic work, please cite:
 
-## License
-This python reimplementation is largely based on [sptam](https://github.com/lrse/sptam), so it's licensed under GPLv3 License. 
+    @article{lin2021rdrslam,
+      title={{RDR_SLAM}: Semantic Segmentaton of Moving Objects  },
+      author={Lin, Peisen},
+      year={2021}
+     }
 
-## Contact
-If you have problems related to the base S-PTAM algorithm, you can contact original authors [lrse](https://github.com/lrse) (robotica@dc.uba.ar), or refer to the related papers:  
-[1]  Taihú Pire,Thomas Fischer, Gastón Castro, Pablo De Cristóforis, Javier Civera and Julio Jacobo Berlles.
-**S-PTAM: Stereo Parallel Tracking and Mapping**
-Robotics and Autonomous Systems, 2017.  
+## Acknowledgements
+Our code builds on [DynaSLAM](https://github.com/BertaBescos/DynaSLAM). [ORB_SLAM2-PythonBindings](https://github.com/jskinn/ORB_SLAM2-PythonBindings).
 
-[2] Taihú Pire, Thomas Fischer, Javier Civera, Pablo De Cristóforis and Julio Jacobo Berlles.  
-**Stereo Parallel Tracking and Mapping for Robot Localization**  
-Proc. of The International Conference on Intelligent Robots and Systems (IROS), Hamburg, Germany, 2015.  
-
-
-This project is based on https://github.com/uoip/stereo_ptam.
--- examples/script.py
+++ examples/script.py
@@ -36,10 +36,16 @@ from maskrcnn_benchmark.config import cfg
 from demo.predictor import COCODemo
 
 from PDSeg import PDSeg
-from sptam.dynaseg import DynaSegt
-from sptam.msptam import stereoCamera
+from sptam.dynaseg import DynaSegt,DynaSeg
+from sptam.msptam import SPTAM, stereoCamera
+from sptam.components import Camera
+from sptam.components import StereoFrame
+from sptam.feature import ImageFeature
+from sptam.params import ParamsKITTI
+from sptam.dataset import KITTIOdometry
 
 import orbslam2
+import g2o
 
 import sys
 import cv2 as cv
@@ -47,6 +53,7 @@ import traceback
 import numpy as np
 import os
 import shutil
+from threading import Thread
 
 def load_images(path_to_sequence):
     res = [os.path.join(path_to_sequence, img) for img in os.listdir(path_to_sequence)]
@@ -73,11 +80,12 @@ def pose_to_transformation(pose):
 sequence = sys.argv[1]
 mode = sys.argv[2]
 
-file_path = os.path.join('/storage/remote/atcremers17/linp/dataset/kittic/sequences/',sequence, 'image_2')
+sequence_path = os.path.join('/storage/remote/atcremers17/linp/dataset/kittic/sequences/',sequence)
+file_path = os.path.join(sequence_path, 'image_2')
 left_filenames = load_images(file_path)
-file_path = os.path.join('/storage/remote/atcremers17/linp/dataset/kittic/sequences/',sequence, 'image_3')
+file_path = os.path.join(sequence_path, 'image_3')
 right_filenames = load_images(file_path)
-timestamps = load_times(os.path.join('/storage/remote/atcremers17/linp/dataset/kittic/sequences/',sequence))
+timestamps = load_times(sequence_path)
 
 prob_path = os.path.join('/usr/stud/linp/storage/user/linp/prob/', sequence)
 prob_filenames = load_images(prob_path)
@@ -98,80 +106,163 @@ kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (2 * dilation + 1, 2 * dilat
 depth_path = os.path.join('/usr/stud/linp/storage/user/linp/depth/',sequence)
 
 iml = cv.imread(left_filenames[0], cv.IMREAD_UNCHANGED)
-if mode == 'dpr' or mode == 'tt':
-    pdseg = PDSeg(iml,coco_demo,depth_path,kernel)
-else:
-    orb_path = '/usr/stud/linp/storage/user/linp/ORB_SLAM2'
-    vocab_path = os.path.join(orb_path, 'Vocabulary/ORBvoc.txt')
-    ins = int(sequence)
-    if ins < 3:
-        settings_path = os.path.join(orb_path, 'Examples/Stereo/KITTI00-02.yaml')
-    elif ins == 3:
-        settings_path = os.path.join(orb_path, 'Examples/Stereo/KITTI03.yaml')
-    else:
-        settings_path = os.path.join(orb_path, 'Examples/Stereo/KITTI04-12.yaml')
-    slam0 = orbslam2.System(vocab_path, settings_path, orbslam2.Sensor.STEREO)
-    slam0.set_use_viewer(False)
-    slam0.initialize()
-    feature_params = dict(maxCorners=1000,
-                          qualityLevel=0.1,
-                          minDistance=7,
-                          blockSize=7)
-    config = stereoCamera()
-    mtx = np.array([[707.0912, 0, 601.8873], [0, 707.0912, 183.1104], [0, 0, 1]])
-    dist = np.array([[0] * 4]).reshape(1, 4).astype(np.float32)
-    lk_params = dict(winSize=(15, 15),
-                     maxLevel=2,
-                     criteria=(cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 0.03))
-    loadmodel = './finetune_300.tar'
-    paraml = {'minDisparity': 1,
-              'numDisparities': 64,
-              'blockSize': 10,
-              'P1': 4 * 3 * 9 ** 2,
-              'P2': 4 * 3 * 9 ** 2,
-              'disp12MaxDiff': 1,
-              'preFilterCap': 10,
-              'uniquenessRatio': 15,
-              'speckleWindowSize': 100,
-              'speckleRange': 1,
-              'mode': cv.STEREO_SGBM_MODE_SGBM_3WAY
-              }
-
-    dseg = DynaSegt(iml, coco_demo, feature_params, depth_path, config, paraml, lk_params, mtx, dist, kernel, loadmodel)
-
-
+f = 0
+config = stereoCamera(sequence)
 num_images = len(left_filenames)
-
-dpath = 'pmask/{}{}/'.format(mode,sequence)
-if os.path.exists(dpath):
-    shutil.rmtree(dpath)
-os.mkdir(dpath)
-
-print('sequence ',sequence)
-for idx in range(num_images):
-    left_image = cv.imread(left_filenames[idx], cv.IMREAD_UNCHANGED)
-    right_image = cv.imread(right_filenames[idx], cv.IMREAD_UNCHANGED)
-    prob_image = cv.imread(prob_filenames[idx])
-    # dpr
-    if mode == 'dpr':
-        c = pdseg.pd_seg_rec(left_image, prob_image,idx)
-        cv.imwrite(os.path.join(dpath, '{0:06}.png'.format(idx)), c*255)
-    # t
-    elif mode == 'tt':
-        c = pdseg.pd_seg_t(left_image, prob_image)
-        cv.imwrite(os.path.join(dpath, '{0:06}.png'.format(idx)), c)
+if mode != 'm':
+    if mode == 'dpr' or mode == 'tt':
+        pdseg = PDSeg(iml,coco_demo,depth_path,kernel,config)
     else:
-        left_mask = np.ones((dseg.h, dseg.w, 1), dtype=np.uint8)
-        right_mask = np.ones((dseg.h, dseg.w, 1), dtype=np.uint8)
-        timestamp = timestamps[idx]
-        slam0.process_image_stereo(left_image[:, :, ::-1], right_image[:, :, ::-1], left_mask, right_mask, timestamp)
-        trans = pose_to_transformation(slam0.get_trajectory_points()[-1])
-        if idx % 3 == 0:
-            if idx:
-                c = dseg.dyn_seg_rec(trans, left_image, idx)
-            dseg.updata(left_image, right_image, idx, trans)
+        feature_params = dict(maxCorners=1000,
+                              qualityLevel=0.1,
+                              minDistance=7,
+                              blockSize=7)
+        mtx = np.array([[707.0912, 0, 601.8873], [0, 707.0912, 183.1104], [0, 0, 1]])
+        dist = np.array([[0] * 4]).reshape(1, 4).astype(np.float32)
+        lk_params = dict(winSize=(15, 15),
+                         maxLevel=2,
+                         criteria=(cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 0.03))
+        loadmodel = './finetune_300.tar'
+        paraml = {'minDisparity': 1,
+                  'numDisparities': 64,
+                  'blockSize': 10,
+                  'P1': 4 * 3 * 9 ** 2,
+                  'P2': 4 * 3 * 9 ** 2,
+                  'disp12MaxDiff': 1,
+                  'preFilterCap': 10,
+                  'uniquenessRatio': 15,
+                  'speckleWindowSize': 100,
+                  'speckleRange': 1,
+                  'mode': cv.STEREO_SGBM_MODE_SGBM_3WAY
+                  }
+        if mode[0] == 'd':
+            f = 1
+            orb_path = '/usr/stud/linp/storage/user/linp/ORB_SLAM2'
+            vocab_path = os.path.join(orb_path, 'Vocabulary/ORBvoc.txt')
+            ins = int(sequence)
+            if ins < 3:
+                settings_path = os.path.join(orb_path, 'Examples/Stereo/KITTI00-02.yaml')
+            elif ins == 3:
+                settings_path = os.path.join(orb_path, 'Examples/Stereo/KITTI03.yaml')
+            else:
+                settings_path = os.path.join(orb_path, 'Examples/Stereo/KITTI04-12.yaml')
+            slam0 = orbslam2.System(vocab_path, settings_path, orbslam2.Sensor.STEREO)
+            slam0.set_use_viewer(False)
+            slam0.initialize()
+            dseg = DynaSegt(iml, coco_demo, feature_params, depth_path, config, paraml, lk_params, mtx, dist, kernel, loadmodel)
         else:
-            c = dseg.dyn_seg_rec(trans, left_image, idx)
-        if idx:
-            cv.imwrite(os.path.join(dpath, '{0:06}.png'.format(idx)), c * 255)
-    print('{} frame'.format(idx))
+            f = 2
+            params = ParamsKITTI()
+            dataset = KITTIOdometry(sequence_path)
+            sptam = SPTAM(params)
+            dseg = DynaSeg(iml, coco_demo, feature_params, depth_path, config, paraml, lk_params, mtx, dist, kernel,
+                           loadmodel)
+            cam = Camera(
+                dataset.cam.fx, dataset.cam.fy, dataset.cam.cx, dataset.cam.cy,
+                dataset.cam.width, dataset.cam.height,
+                params.frustum_near, params.frustum_far,
+                dataset.cam.baseline)
+
+
+
+
+    dpath = 'pmask/{}{}/'.format(mode,sequence)
+    if os.path.exists(dpath):
+        shutil.rmtree(dpath)
+    os.mkdir(dpath)
+
+    print('sequence ',sequence)
+    print(mode)
+
+
+
+    for idx in range(num_images):
+        print('{} frame'.format(idx))
+        left_image = cv.imread(left_filenames[idx], cv.IMREAD_UNCHANGED)
+        right_image = cv.imread(right_filenames[idx], cv.IMREAD_UNCHANGED)
+        prob_image = cv.imread(prob_filenames[idx])
+        # dpr
+        if mode == 'dpr':
+            c = pdseg.pd_seg_rec(left_image, prob_image,idx)
+            cv.imwrite(os.path.join(dpath, '{0:06}.png'.format(idx)), c*255)
+            # cc = pdseg.pd_seg_rec(left_image, prob_image, idx)
+            # c = np.zeros(left_image.shape[:2])
+            # for obj in pdseg.obj:
+            #     if obj[2]:
+            #         c[obj[0]] = 255
+            #         print(obj[5])
+            # cv.imwrite(os.path.join(dpath, '{0:06}.png'.format(idx)), c)
+        # t
+        elif mode == 'tt':
+            c = pdseg.pd_seg_t(left_image, prob_image)
+            cv.imwrite(os.path.join(dpath, '{0:06}.png'.format(idx)), c)
+        else:
+            timestamp = timestamps[idx]
+            if mode[0] == 'd':
+                left_mask = np.ones((dseg.h, dseg.w, 1), dtype=np.uint8)
+                right_mask = np.ones((dseg.h, dseg.w, 1), dtype=np.uint8)
+                slam0.process_image_stereo(left_image[:, :, ::-1], right_image[:, :, ::-1], left_mask, right_mask, timestamp)
+                trans = pose_to_transformation(slam0.get_trajectory_points()[-1])
+                if idx % 3 == 0:
+                    if idx:
+                        if mode == 'dsr':
+                            c = dseg.dyn_seg_rec(trans, left_image, idx)
+                        else:
+                            c = dseg.dyn_seg(trans, left_image)
+                    dseg.updata(left_image, right_image, idx, trans)
+                else:
+                    if mode[:3] == 'dsr':
+                        c = dseg.dyn_seg_rec(trans, left_image, idx)
+                    else:
+                        c = dseg.dyn_seg(trans, left_image)
+                if idx:
+                    cv.imwrite(os.path.join(dpath, '{0:06}.png'.format(idx)), c * 255)
+            else:
+                featurel = ImageFeature(left_image, params)
+                featurer = ImageFeature(right_image, params)
+                t = Thread(target=featurer.extract)
+                t.start()
+                featurel.extract()
+                t.join()
+                frame = StereoFrame(idx, g2o.Isometry3d(), featurel, featurer, cam, timestamp=timestamp)
+
+                if not sptam.is_initialized():
+                    sptam.initialize(frame)
+                else:
+                    sptam.track(frame)
+
+                if idx % 3 == 0:
+                    if idx:
+                        c = dseg.dyn_seg(frame, left_image)
+                    dseg.updata(left_image, right_image, idx, frame)
+                else:
+                    c = dseg.dyn_seg(frame, left_image)
+                if idx:
+                    c = cv.dilate(c, kernel)
+                    cv.imwrite(os.path.join(dpath, '{0:06}.png'.format(idx)), c * 255)
+
+
+
+    if mode != 'dpr' and mode != 'tt':
+        print('mean dcverror: {}'.format(np.mean(dseg.cverrs)))
+    if f == 1:
+        slam0.shutdown()
+    elif f == 2:
+        sptam.stop()
+else:
+    dpath = 'mask/o{}/'.format(sequence)
+    if os.path.exists(dpath):
+        shutil.rmtree(dpath)
+    os.mkdir(dpath)
+
+    print('sequence ',sequence)
+    for idx in range(num_images):
+        print('{} frame'.format(idx))
+        left_image = cv.imread(left_filenames[idx], cv.IMREAD_UNCHANGED)
+        a = coco_demo.compute_prediction(left_image)
+        top = coco_demo.select_top_predictions(a)
+        masks = top.get_field("mask").numpy()
+        c = np.zeros(left_image.shape[:2])
+        for m in masks:
+            c[m.squeeze()] = 255
+        cv.imwrite(os.path.join(dpath, '{0:06}.png'.format(idx)), c)
